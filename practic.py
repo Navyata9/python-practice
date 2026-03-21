33c470f113690bea8eea80039f25882bbd8fbd74
Navyata9 <navyatavelaga@gmail.com>
2026-03-21 10:24:22 -0500

    Add files via upload

---
+++ practic.py
@@ -0,0 +1,12 @@
+num1=int(input("Enter the first number: "))
+num2=int(input("Enter the second number:"))
+sum=num1+num2
+multiply=num1*num2
+subtract=num1-num2
+div=num1/num2
+print(sum)
+print(multiply)
+print(subtract)
+print(div)
+stuff=sum+multiply+subtract+div
+print(stuff)
