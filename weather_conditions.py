54930e817f3241f962f20b35f08bf6b5911da428
Navyata9 <navyatavelaga@gmail.com>
2026-06-20 12:22:28 -0500

    Add files via upload

---
+++ weather_conditions.py
@@ -0,0 +1,8 @@
+def weather_conditions():
+    print("the weather is pleasent in", spring)
+    print("the weather is the same in", autumn)
+
+spring= "autumn"
+autumn= spring
+
+weather_conditions()
