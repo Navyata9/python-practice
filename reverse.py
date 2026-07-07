b5cfe6d02ada438d04c4bb2c2c940822554d40a8
Navyata9 <navyatavelaga@gmail.com>
2026-07-07 09:01:12 -0500

    Add files via upload

--- reverse.py
+++ reverse.py
@@ -1,7 +1,7 @@
-text= str(input("Enter a string:"))
-
-revText= text[::-1]
-text = revText
-
-print("Reverse of Given String is:")
-print (text)
+var=10
+while var>0:
+    var= var-1
+    if var==5:
+        continue
+    print("\nCurrent variable value:", var)
+print("\nGood bye!")
